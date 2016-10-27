e976c3c0a4e2de3c5ada31df030f2a9814e129fb
annam95 <anna.mc-cann@ucdconnect.ie>
2016-10-28 00:46:48 +0100

    Initial implementation of insert

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
@@ -11,8 +11,40 @@ private:
 	Node* right_m;
 	int value_m;
 public:
-	void insert(int value);
+	void insert(int value)
+	{
+		if (value < value_m)
+		{
+			if (left_m == nullptr)
+			{
+				left_m->value_m = value;
+			}
+			else
+			{
+				*left_m.insert(value);
+			}
+		}
+		else if (value > value_m)
+		{
+			if (right_m == nullptr)
+			{
+				right_m->value_m = value;
+			}
+			else
+			{
+				*right_m.insert(value);
+			}
+		}
+		else
+		{
+			// what to do if value == value_m?
+		}
+	}
+
 	bool contains(int value);
+	{
+
+	}
 	void printInOrder();
 };
 
